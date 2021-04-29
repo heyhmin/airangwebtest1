97df07b91783769678f44e43f749314e94764d5a
heyhmin <74590675+heyhmin@users.noreply.github.com>
2021-04-29 17:49:47 +0900

    add / to api url

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 //const API_URL = 'http://localhost:8080/api/test/';
-const API_URL = "https://airang-web-test1.herokuapp.com";
+const API_URL = "https://airang-web-test1.herokuapp.com/";
 
 class UserService {
   getPublicContent() {
